ab5c082fe94e00547983e00d4abd40a6f99c6241
swanand91 <swanandjoshi871@gmail.com>
2017-02-04 16:12:37 +0530

    Created by Coding Ground Client

---
+++ New Project-20170204/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int sub1,sub2,sub3;
+    cout<<"Enter Mathematics marks = ";
+    cin>>sub1;
+    cout<<"Enter Computer marks = ";
+    cin>>sub2;
+    cout<<"Enter Chemistry marks = ";
+    cin>>sub3;
+    cout<<"\nSubject\t\t\tMarks\n";
+    cout<<"\nMathematics\t\t"<<sub1;
+    cout<<"\nComputer\t\t"<<sub2;
+    cout<<"\nChemistry\t\t"<<sub3;
+    getch();
+}
